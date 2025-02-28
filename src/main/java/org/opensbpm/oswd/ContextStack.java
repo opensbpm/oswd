cfd7d1e06353aec5732f912aaf7a4027a38b4eb0
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-02-28 16:05:21 +0100

    Test subject name

---
+++ src/main/java/org/opensbpm/oswd/ContextStack.java
@@ -0,0 +1,38 @@
+package org.opensbpm.oswd;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import org.opensbpm.oswd.OswdParser.ProcessContext;
+import org.opensbpm.oswd.OswdParser.SubjectContext;
+import org.opensbpm.oswd.ModelBuilderFactory.ProcessBuilder;
+import org.opensbpm.oswd.ModelBuilderFactory.SubjectBuilder;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.opensbpm.oswd.ModelBuilderFactory.ModelBuilder;
+
+public class ContextStack {
+    private Map<
+            ParserRuleContext,
+            Stack<? extends ModelBuilder>
+            > classStackMap = new HashMap<>();
+
+    public <B extends ModelBuilder> B register(StackItem<B, ? extends ParserRuleContext> stackItem) {
+        Stack<B> stack = new Stack<>();
+        classStackMap.putIfAbsent(stackItem.getContext(), stack);
+        return stack.push(stackItem.createBuilder());
+    }
+
+    public <B extends ModelBuilder> B peek(StackItem<B, ? extends ParserRuleContext> stackItem) {
+        return getStack(stackItem).peek();
+    }
+
+
+    public <B extends ModelBuilder> B pop(StackItem<B, ? extends ParserRuleContext> stackItem) {
+        return getStack(stackItem).pop();
+    }
+
+    private <B extends ModelBuilder> Stack<B> getStack(StackItem<B, ? extends ParserRuleContext> stackItem) {
+        return (Stack<B>) classStackMap.get(stackItem.getContext());
+    }
+
+}
